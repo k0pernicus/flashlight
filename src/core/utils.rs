e1be22b4ef46470967e3b9a5d87fac41f377fb42
k0pernicus <antonin.carette@gmail.com>
2016-03-19 21:15:37 -0400

    Better way to open the file which has been found

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,11 +1,10 @@
 use std::process::Command;
+use std::process::Stdio;
 
 /// Open a file with the ```open``` command system
-fn open_file(file : &str) {
+pub fn open_file(file : &str) {
 
-    let command_to_execute = "xdg-open".to_string() + file;
-
-    match Command::new(command_to_execute).status() {
+    match Command::new("xdg-open").arg(file).stdout(Stdio::null()).stderr(Stdio::null()).status() {
         Ok(_) => (),
         Err(e) => println!("failed to execute process: {}", e),
     }
