61b02b14f6c7b99fb6b7f1def5d49199014a6305
k0pernicus <antonin.carette@gmail.com>
2016-03-27 21:42:38 -0400

    Non-indexation of cached files

--- src/core/searching.rs
+++ src/core/searching.rs
@@ -18,8 +18,13 @@ pub fn search_file_in_db(indexed_documents: &IndexedDocuments, file : &str) -> V
 
 pub fn get_nb_of_child(directory : &PathBuf) -> usize {
     let mut nb_of_child : usize = 1;
-    for _ in WalkDir::new(directory).follow_links(true).into_iter().filter_map(|e| e.ok()) {
-        nb_of_child += 1;
+    for entry in WalkDir::new(directory).follow_links(true).into_iter().filter_map(|e| e.ok()) {
+        let entry_path = entry.path();
+        let entry_path_str = entry_path.to_str().unwrap();
+        let split_document_path = entry_path_str.split("/").collect::<Vec<&str>>();
+        if !split_document_path.iter().fold(false, |contains_cached, file| contains_cached || file.starts_with(".")) {
+            nb_of_child += 1;
+        }
     }
     nb_of_child
 }
@@ -42,32 +47,40 @@ pub fn scan_repositories(directory : &PathBuf, indexed_documents : &mut IndexedD
             // Get informations about the document
             let document_size : u64 = entry_path.metadata().unwrap().len();
             let split_document_path = entry_path_str.split("/").collect::<Vec<&str>>();
-            let document_path = split_document_path[0..split_document_path.len() - 1].join("/");
-            let document_filename = split_document_path[split_document_path.len() - 1];
-            let document_filename_s = document_filename.split(".").collect::<Vec<&str>>();
-            let mut document_extension = "";
-            let mut document_filename_without_extension = document_filename.to_string();
-            // Just modify the 2 last fields if the length of the ```document_filename_s``` field is different than 1 (no split)
-            if document_filename_s.len() != 1 {
-                document_extension = document_filename_s[document_filename_s.len() - 1];
-                document_filename_without_extension = document_filename_s[0..(document_filename_s.len() - 1)].join(".");
-            }
 
-            let new_document = Document::new(document_extension,
-                                             &document_filename_without_extension,
-                                             &document_path,
-                                              document_size);
+            // Index only non-cached files
+            if ! split_document_path.iter().fold(false, |contains_cached, file| contains_cached || file.starts_with(".")) {
 
-            indexed_documents.add_path(entry_path_str);
-            if ! indexed_documents.get_core().contains_key(document_filename) {
-                indexed_documents.create_doc_in_core(document_filename);
+                let document_path = split_document_path[0..split_document_path.len() - 1].join("/");
+                let document_filename = split_document_path[split_document_path.len() - 1];
+                let document_filename_s = document_filename.split(".").collect::<Vec<&str>>();
+                let mut document_extension = "";
+                let mut document_filename_without_extension = document_filename.to_string();
+
+
+                // Just modify the 2 last fields if the length of the ```document_filename_s``` field is different than 1 (no split)
+                if document_filename_s.len() != 1 {
+                    document_extension = document_filename_s[document_filename_s.len() - 1];
+                    document_filename_without_extension = document_filename_s[0..(document_filename_s.len() - 1)].join(".");
+                }
+
+                let new_document = Document::new(document_extension,
+                    &document_filename_without_extension,
+                    &document_path,
+                    document_size);
+
+                indexed_documents.add_path(entry_path_str);
+                if ! indexed_documents.get_core().contains_key(document_filename) {
+                    indexed_documents.create_doc_in_core(document_filename);
+                }
+                indexed_documents.add_doc_in_core(document_filename, new_document);
+                indexed_documents.add_doc_in_core_vector(document_filename);
+
+                print!("\rIndexed files: {0} / {1} files...", current_child, nb_of_child);
+                io::stdout().flush().ok().expect("Could not flush stdout");
+                current_child += 1;
             }
-            indexed_documents.add_doc_in_core(document_filename, new_document);
-            indexed_documents.add_doc_in_core_vector(document_filename);
         }
-        print!("\rIndexed files: {0} / {1} files...", current_child, nb_of_child);
-        io::stdout().flush().ok().expect("Could not flush stdout");
-        current_child += 1;
     }
     indexed_documents.sort_core_vector();
 }
